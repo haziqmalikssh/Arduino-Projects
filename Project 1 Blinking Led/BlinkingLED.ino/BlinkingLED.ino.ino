92e9a5e958c7b1cd7264328e2ebcd0b1081428ac
haziqmalikssh <haziqmalik2022@gmail.com>
2025-08-12 16:26:17 -0400

    Blinking Led light code

---
+++ Project 1 Blinking Led/BlinkingLED.ino/BlinkingLED.ino.ino
@@ -0,0 +1,16 @@
+/*
+  Blink Example for Arduino Mega
+*/
+
+const int ledPin = 13; // Onboard LED pin on Arduino Mega
+
+void setup() {
+  pinMode(ledPin, OUTPUT); // Set pin as output
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH); // Turn LED on
+  delay(1000);                // Wait 1 second
+  digitalWrite(ledPin, LOW);  // Turn LED off
+  delay(1000);                // Wait 1 second
+}
