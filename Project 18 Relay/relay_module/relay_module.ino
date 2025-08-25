ab14e66381824b9b243244bdd3ecb4c13c7d3a9e
haziqmalikssh <haziqmalik2022@gmail.com>
2025-08-25 19:35:09 -0400

    Create relay_module.ino

---
+++ Project 18 Relay/relay_module/relay_module.ino
@@ -0,0 +1,19 @@
+// Relay pin connected to Arduino
+int relayPin = 7;  // Use the pin you wired to relay input
+
+void setup() {
+  pinMode(relayPin, OUTPUT); // Set relay pin as output
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Turn relay ON (energize coil, motor driver powered)
+  digitalWrite(relayPin, HIGH);
+  Serial.println("Relay ON - Motor driver powered");
+  delay(5000); // wait 5 seconds
+
+  // Turn relay OFF (de-energize coil, motor driver disconnected)
+  digitalWrite(relayPin, LOW);
+  Serial.println("Relay OFF - Motor driver disconnected");
+  delay(5000); // wait 5 seconds
+}
