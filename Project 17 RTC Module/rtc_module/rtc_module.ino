04dd57deeb2175f85299325842ac702932b30dfd
haziqmalikssh <haziqmalik2022@gmail.com>
2025-08-24 20:10:34 -0400

    Create rtc_module.ino

---
+++ Project 17 RTC Module/rtc_module/rtc_module.ino
@@ -0,0 +1,40 @@
+#include <Wire.h>
+#include <RTClib.h>
+
+RTC_DS1307 rtc;
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+
+  if (!rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+
+  if (!rtc.isrunning()) {
+    Serial.println("RTC is NOT running, setting the time...");
+    // Set RTC time to compile time (upload time)
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+}
+
+void loop() {
+  DateTime now = rtc.now();
+
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print(" ");
+
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.print(now.second(), DEC);
+  Serial.println();
+
+  delay(1000);
+}
