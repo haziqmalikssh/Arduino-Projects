3eba45ea6647bf30ca843b128316a340e0ab2162
haziqmalikssh <haziqmalik2022@gmail.com>
2025-08-30 15:57:07 -0400

    code

--- Project 22 Radio Transmit and Recieve/reciever/reciever.ino
+++ Project 22 Radio Transmit and Recieve/reciever/reciever.ino
@@ -2,22 +2,23 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
+// CE, CSN pins
 RF24 radio(9, 10);  
-const byte address[6] = "00001";
+const byte address[6] = "00001";  
 
 void setup() {
   Serial.begin(9600);
   radio.begin();
   radio.openReadingPipe(0, address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.startListening();
+  radio.setPALevel(RF24_PA_HIGH);  
+  radio.startListening();  // Set as receiver
 }
 
 void loop() {
   if (radio.available()) {
     char text[32] = "";
     radio.read(&text, sizeof(text));
-    Serial.print("Received: ");
+    Serial.print("Message Received: ");
     Serial.println(text);
   }
 }
--- Project 22 Radio Transmit and Recieve/transmitter/transmitter.ino
+++ Project 22 Radio Transmit and Recieve/transmitter/transmitter.ino
@@ -2,20 +2,21 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-RF24 radio(9, 10);  // CE, CSN pins
-const byte address[6] = "00001"; // communication pipe
+// CE, CSN pins
+RF24 radio(9, 10);  
+const byte address[6] = "00001";  
 
 void setup() {
   Serial.begin(9600);
   radio.begin();
   radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();
+  radio.setPALevel(RF24_PA_HIGH);  
+  radio.stopListening();  // Set as transmitter
 }
 
 void loop() {
-  const char text[] = "Hello from A";
+  const char text[] = "Hello from Arduino!";
   radio.write(&text, sizeof(text));
-  Serial.println("Sent: Hello from A");
+  Serial.println("Message Sent: Hello from Arduino!");
   delay(1000);
 }
